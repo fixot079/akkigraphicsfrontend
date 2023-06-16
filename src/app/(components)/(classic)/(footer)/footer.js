57382633ed808a90fb89753746cf9db3214d70bd
fixot079 <133486611+fixot079@users.noreply.github.com>
2023-06-16 20:29:34 +0530

    Update footer.js

--- src/app/(components)/(classic)/(footer)/footer.js
+++ src/app/(components)/(classic)/(footer)/footer.js
@@ -1,36 +1,37 @@
 import React, {useState, useEffect} from 'react'
 import Imagecomp from '../(image)/imagecomp';
-
+import './footer.css'
 const Footer = () => {
-    const  backendaddress = "https://akkigraphicsbackend.onrender.com"
+
+    // Address
+    const backendaddress = "https://akkigraphicsbackend.onrender.com"
+
+    // States
     const [footerLogo, setFooterLogo] = useState([])
     const [footerLogoIsLoading, setFooterLogoIsLoading] = useState(true)
     const [footerClass, setFooterClass] = useState("")
     const [footerStyle, setFooterStyle] = useState({})
+
+    // Use Effect
     useEffect(()=>{
       fetch(`${backendaddress}/getimages/footerlogo`)
       .then(response => response.json())
       .then(data => {
-        // Process the received image names
         const images = data.images;
         setFooterLogo(images)
         setFooterLogoIsLoading(false)
       })
-      .catch(error => {
-        console.error(error);
-        // Handle any errors that occur during the fetch request
-      });
     },[])
-
-        
+ 
     return (
         <>
-        <footer id="footer" className={footerClass} style={footerStyle}>
-            {!footerLogoIsLoading && <Imagecomp source={`${backendaddress}/images/footerLogo/`+footerLogo[0]} id="footer-logo"/>}
-            <p className="inter footertext">&copy; Akki Graphics</p>
-        </footer>  
+          <footer id="footer" className={footerClass} style={footerStyle}>
+              {!footerLogoIsLoading && <Imagecomp source={`${backendaddress}/images/footerLogo/`+footerLogo[0]} id="footer-logo"/>}
+              <p className="inter footertext">&copy; Akki Graphics</p>
+          </footer>  
         </>
     )
 }
 
 export default Footer
+
