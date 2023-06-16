4c96a9442b302d67a52ee8ede1715ec3e68502da
fixot079 <133486611+fixot079@users.noreply.github.com>
2023-06-16 19:39:58 +0530

    Update page.js

--- src/app/(pages)/management/page.js
+++ src/app/(pages)/management/page.js
@@ -3,40 +3,36 @@ import React, {useState, useEffect} from 'react'
 import './management.css'
 
 const Management = () => {
+  // Address
+  const backendaddress = "https://akkigraphicsbackend.onrender.com"
+
+  // States
+    const [textObject, setTextObject] = useState({})
+
+  // Use Effect
+    useEffect(()=>{
+        fetch(`${backendaddress}/management/get`)
+        .then((r) => r.json())
+        .then((r) => {
+          setTextObject(r);
+      })
+    },[])
     return (
         <>
-        <div className="managementsection">
-        <h1 className="ppins mtxt1">Experience and Achievements in Managing Soul Omega Exclusive</h1><br />
-            <p className="inter mtxt2">Welcome to the Management page of my personal website! Here, I would like
-             to share my experiences and accomplishments in managing the Soul Omega Exclusive YouTube channel for
-              the past two months. During my tenure, I had the privilege of overseeing various aspects of the channel's
-               operations, including video production, content planning, strategy implementation, and administration. 
-               <br /><br /> One of my primary responsibilities was managing the daily upload of three videos, along with
-                creating captivating thumbnails that would attract viewers. In addition to the technical aspects, I also 
-                provided guidance and direction to a team of editors. It was my role to ensure that they understood the
-                vision for each video and executed the necessary edits to align with the channel's style and goals. 
-                Through effective communication and clear direction, I successfully coordinated their efforts and 
-                maintained consistent quality across all content. As part of my strategic planning, I carefully
-                determined the optimal timing for video releases, considering factors such as audience 
-                engagement patterns and trending topics. This involved planning the sequence of uploads,
-                determining appropriate titles for each video, and creating eye-catching thumbnails that 
-                would entice potential viewers to click. <br /> <br />Thanks to our collective efforts
-                and meticulous planning, the channel experienced significant growth, acquiring an impressive
-                1200 new subscribers during my tenure. This milestone is a testament to the successful 
-                implementation of our content strategies and the dedication of the team involved.
-                In my role as a manager, I also handled various administrative tasks related to the channel's 
-                operations. This included managing the channel's schedule, coordinating collaborations with 
-                other creators, and responding to comments and inquiries from the audience. I ensured that all 
-                activities ran smoothly, allowing for a seamless experience for both the team and our viewers.
-                <br /><br />Overall, my experience managing the Soul Omega Exclusive YouTube channel has
-                provided me with invaluable insights into content planning, strategy development,
-                administration, and direction. I am proud of the growth and achievements we accomplished 
-                during this time, and I look forward to applying my skills and expertise to future endeavors.
-                Thank you for visiting, and please feel free to explore the other sections to learn more about
-                my journey and experiences.</p>
-        </div>
+            <div className="managementsection">
+            <h1 className="ppins mtxt1">{textObject.h1}</h1><br />
+                <p className="inter mtxt2">{textObject.p1}
+                <br /><br /> 
+                {textObject.p2} 
+                <br /><br /> 
+                {textObject.p3} 
+                <br /><br />
+                {textObject.p4}
+                </p>
+            </div>
         </>
     )
 }
 
 export default Management
+
