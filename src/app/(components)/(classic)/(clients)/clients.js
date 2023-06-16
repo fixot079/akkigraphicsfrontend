2aa1c7dd8e7d4641b536195559065b2d05c64661
fixot079 <133486611+fixot079@users.noreply.github.com>
2023-06-16 20:28:33 +0530

    Update clients.js

--- src/app/(components)/(classic)/(clients)/clients.js
+++ src/app/(components)/(classic)/(clients)/clients.js
@@ -1,31 +1,32 @@
 import React, {useState, useEffect} from 'react'
+import { removeExtensionsFromName } from '../../(admin)/(dashboard)/dashboard'
 import Imagecomp from '../(image)/imagecomp'
 import Spinner from '../(spinner)/spinner'
-import { removeExtensionsFromName } from '../../(admin)/(dashboard)/dashboard'
 import '../../../(pages)/overview/overview.css'
+
 const Clients = () => {
-  const  backendaddress = "https://akkigraphicsbackend.onrender.com"
+  // Address
+    const backendaddress = "https://akkigraphicsbackend.onrender.com"
+
+  // States
     const [isLoading, setIsLoading] = useState(true)
     const [imgArray, setImgArray] = useState([])
 
+  // Use Effect
     useEffect(()=>{
         fetch(`${backendaddress}/getimages/clients`)
         .then(response => response.json())
         .then(data => {
-          // Access the array of image names
           const imageNames = data.images;
   
-          // Process the image names as needed
           setImgArray(imageNames);
           setIsLoading(false)
         })
-        .catch(error => {
-          console.error('Request failed:', error);
-        });
     },[])
+
     return (
         <>
-        {/* {isLoading && <Spinner />} */}
+        {isLoading && <Spinner />}
          {
           imgArray.map((imagesource)=>{
             return <div className="container" key={Date.now()+Math.random()*9999}><Imagecomp source={`${backendaddress}/images/clients/` + imagesource} classes="client-image" key={Date.now() + imagesource}/>
@@ -37,3 +38,4 @@ const Clients = () => {
 }
 
 export default Clients
+
